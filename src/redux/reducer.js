270176a7650c70180fc6c4618cb942b97cb5461e
ABHISHEK KUMAR <amreshk005@gmail.com>
2021-07-28 08:35:18 +0530

    added structure for redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,41 @@
+const todoAppState = {
+  todos: [
+    { id: 0, text: "Learn React", completed: true },
+    { id: 1, text: "Learn Redux", completed: false, color: "purple" },
+    { id: 2, text: "Build something fun!", completed: false, color: "blue" },
+  ],
+  filters: {
+    status: "Active",
+    colors: ["red", "blue"],
+  },
+};
+
+export default function todosReducer(state = initialState, action) {
+  switch (action.type) {
+    case "todos/todoAdded": {
+      // Can return just the new todos array - no extra object around it
+      return [
+        ...state,
+        {
+          id: nextTodoId(state),
+          text: action.payload,
+          completed: false,
+        },
+      ];
+    }
+    case "todos/todoToggled": {
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+    }
+    default:
+      return state;
+  }
+}
